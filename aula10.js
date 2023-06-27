48a600c6d4233b30f770ee368326ca29ea8c9bb9
Aline-Schlindwein <136762994+Aline-Schlindwein@users.noreply.github.com>
2023-06-27 19:40:00 -0300

    Add files via upload

--- aula10.js
+++ aula10.js
@@ -1,6 +1,5 @@
 var prompt = require('prompt-sync')();
 
-var i = 0;
 var cardapio = {100: 1.20,  101: 1.30,  102: 1.50,  103: 1.20,  104: 1.30,  105: 1.00};
 var totalPedido = 0;
 var itensComprados = [];
@@ -20,7 +19,7 @@ while (true) {
     case 104:
     case 105:
       var quantidade;
-      do {
+      do{
         quantidade = parseInt(prompt('Digite a quantidade desejada: '));
         if (isNaN(quantidade)) {
           console.log('A quantidade deve ser um número. Tente novamente.');
@@ -30,15 +29,23 @@ while (true) {
       var precoItem = cardapio[codigo];
       var valorItem = precoItem * quantidade;
       totalPedido += valorItem;
-      console.log('Cod:', codigo, 'Quantidade:', quantidade, 'valor por item:', valorItem);
+      console.log('Cod:', codigo, 'Quantidade:', quantidade, 'valor por item:', valorItem.toFixed(2));
 
+      var itemIgual = itensComprados.find(function(item){
+        return item.codigo === codigo;
+      });
+
+      if (itemIgual) {
+        itemIgual.quantidade += quantidade;
+        itemIgual.valorItem += valorItem;
+
+      } else {
       var item = {
         codigo: codigo,
         quantidade: quantidade,
-        valorItem: valorItem.toFixed(2)
-      };
-
-      itensComprados.push(item);
+        valorItem: valorItem
+      }
+      itensComprados.push(item);};
       break;
 
     default:
@@ -48,7 +55,7 @@ while (true) {
 }
 
 console.log('Itens comprados:');
-for (var j = 0; j < itensComprados.length; j++) {
+for (var j = 0; j<itensComprados.length; j++) {
 console.log('Cod:', itensComprados[j].codigo, 
 'Quantidade:', itensComprados[j].quantidade, 
 'Valor:', itensComprados[j].valorItem);
